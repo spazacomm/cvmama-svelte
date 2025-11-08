3db81d2972d59f29e106238eada0d4b103f27d1e
Jack Chumo <jacqchumo@gmail.com>
2025-11-08 19:56:39 +0300

    chnages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/site.css', 'resources/js/site.js'],
             refresh: true,
+        }),
+        viteStaticCopy({
+            targets: [
+              {
+                src: 'public/sandbox',   // folder to copy
+                dest: ''                 // copies into build output root
+              },
+            ],
         }),
         tailwindcss(),
     ],
